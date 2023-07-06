fab278db715f16865a978f3d754c6fc06b87dbf3
PedroGoncalves77 <117955845+PedroGoncalves77@users.noreply.github.com>
2023-07-06 12:31:27 -0300

    Update DadosDaConexao.cs

--- DAL/DadosDaConexao.cs
+++ DAL/DadosDaConexao.cs
@@ -8,7 +8,7 @@ namespace DAL
 {
     public class DadosDaConexao
     {
-        public static String StringDaConexao { get { return "Data Source=DESKTOP-PLAGVJ7\\SQLEXPRESS1;Initial Catalog=\"Controle de estoque\";Persist Security Info=True;User ID=SQLEXPRESS1;Password=188888"; } }
+        public static String StringDaConexao { get { return "Data Source=DESKTOP-PLAGVJ7\\SQLEXPRESS1;Initial Catalog=\"Controle de estoque\";Persist Security Info=True;User ID=SQLEXPRESS1;Password=******"; } }
   
     }
 
